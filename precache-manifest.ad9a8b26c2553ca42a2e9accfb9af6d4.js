5e6b24dbd580c1d9d0fbbbb486a187babaf4af8c
aznaqCre18 <azizaqibs@gmail.com>
2020-06-26 20:28:45 +0700

    my web

---
+++ precache-manifest.ad9a8b26c2553ca42a2e9accfb9af6d4.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "965c62e555a430d92fc67510b1e10c8c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d4e53dd92f57bf2d7f8",
+    "url": "/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "bf5b55ba51afa62a6adb",
+    "url": "/static/css/main.da335af9.chunk.css"
+  },
+  {
+    "revision": "0d4e53dd92f57bf2d7f8",
+    "url": "/static/js/2.5be62523.chunk.js"
+  },
+  {
+    "revision": "c344ff0e273d3dd0833ad3a0d4c0857e",
+    "url": "/static/js/2.5be62523.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf5b55ba51afa62a6adb",
+    "url": "/static/js/main.a24e34bc.chunk.js"
+  },
+  {
+    "revision": "20d3e938a879d5fbc425",
+    "url": "/static/js/runtime-main.a18f7d93.js"
+  },
+  {
+    "revision": "ede6fdf4ecf8c32c21701823004343f4",
+    "url": "/static/media/Heebo-Bold.ede6fdf4.ttf"
+  },
+  {
+    "revision": "ce8730e7827b824adada73f19b8d5359",
+    "url": "/static/media/Heebo-Light.ce8730e7.ttf"
+  },
+  {
+    "revision": "6c57263dbdee325394e471a12499e64c",
+    "url": "/static/media/Heebo-Medium.6c57263d.ttf"
+  },
+  {
+    "revision": "56936776d7f65473490a55c541ee3451",
+    "url": "/static/media/Heebo-Regular.56936776.ttf"
+  },
+  {
+    "revision": "0f80674363bd9aedee7b983c38735696",
+    "url": "/static/media/Heebo-Thin.0f806743.ttf"
+  },
+  {
+    "revision": "a86d8303f52e0fdd25ed42286a60648e",
+    "url": "/static/media/LINK.a86d8303.svg"
+  },
+  {
+    "revision": "6580104296223ea51e4264a1d134a003",
+    "url": "/static/media/academy.65801042.svg"
+  },
+  {
+    "revision": "9270d226cea524d2cbd36cfb97cae531",
+    "url": "/static/media/breednder.9270d226.svg"
+  },
+  {
+    "revision": "75d6674e711e266581a956f458fbcccd",
+    "url": "/static/media/chiketto.75d6674e.svg"
+  },
+  {
+    "revision": "e789177ca368ffd09105457bcb3f6dd2",
+    "url": "/static/media/donation.e789177c.svg"
+  },
+  {
+    "revision": "afad963931d8e5ca118923713a437f1c",
+    "url": "/static/media/dribble.afad9639.svg"
+  },
+  {
+    "revision": "cb5c12ed30a96bb2af21d47ebe9a4d9d",
+    "url": "/static/media/github.cb5c12ed.svg"
+  },
+  {
+    "revision": "2d094af16c184757153806350e3b3085",
+    "url": "/static/media/linkedin.2d094af1.svg"
+  },
+  {
+    "revision": "c9edb71de9fdd86de7748a6b55e7542a",
+    "url": "/static/media/medium.c9edb71d.svg"
+  },
+  {
+    "revision": "75e8786ed2958cee52dbe33e0d45f7ab",
+    "url": "/static/media/pokemon.75e8786e.svg"
+  },
+  {
+    "revision": "d0bc312902bdf932862e0dfb21a16149",
+    "url": "/static/media/profile.d0bc3129.svg"
+  },
+  {
+    "revision": "539b79deb810320309330cb39afa626b",
+    "url": "/static/media/right-arrow.539b79de.svg"
+  },
+  {
+    "revision": "f02ffbeaafc8f3db40ebfbeb4cfbcada",
+    "url": "/static/media/sandbox.f02ffbea.svg"
+  },
+  {
+    "revision": "f0ee41b0c6c58aeb9dd1765477778cb0",
+    "url": "/static/media/sayurbox.f0ee41b0.svg"
+  }
+]);
